9d5811edd28000b9c86befbf723300d01922e6dd
Ryan Cabanas <ryangithub777@gmail.com>
2022-09-05 22:15:03 -0400

    Lesson #314:
    - Set up automatic retrieval of the stored data.  Initially, this was
      just the token, but we're now saving the remaining time on the token
      too.
    
    - In the `loginHandler`, save the expiration time in `localStorage`.
      I'm saving it as a millisecond integer (which really gets saved as a
      string), while the instructor is saving it as a date string.
    
      Also, here, set a global logout timer based upon the remaining time on
      the token.  Automatically call the `logoutHandler` once the timer runs
      out.
    
    - In the `logoutHandler`, clear values from `localStorage` and clear the
      timer, if one exists.
    
    - Create a `retrieveStoredData` function that gets data from
      `localStorage` and gets the remaining time left on the token. If
      there's less than 1 minute left on the token, go ahead and clear out
      everything from `localStorage` (I'm not sure why we do this) and
      return `null`.  Otherwise, return the data we got from `localStorage`.
    
    - Now, upon every rendering of this `AuthContextProvider` component,
      get the stored data and save the token, if there is one, to the
      initial token state. (The state always clears on page refreshes, so
      we need to do this every time in case the user refreshes the page,
      manually types in URL paths, opens up another browser tab, etc.)
    
    - Lastly, in order to set a timer whenever the token is retrieved from
      `localStorage`, since the timer calls upon the `logoutHandler` to log
      the user out automatically at the end of the token's life, we need to
      use a `useEffect` hook and put it after the `logoutHandler` function
      definition, since we're referencing that function for the timer.

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+let logoutTimer;
 
 const AuthContext = React.createContext({
   token: '',
@@ -15,27 +17,66 @@ const calculateRemainingTime = (expirationTime) => {
   return remainingDuration;
 };
 
+const retrieveStoredData = () => {
+  const storedToken = localStorage.getItem('token');
+  const storedExpirationTime = parseInt(localStorage.getItem('expirationTime'));
+
+  const remainingTime = calculateRemainingTime(storedExpirationTime);
+
+  if (remainingTime <= 60000) {
+    localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+    return null;
+  }
+
+  return {
+    token: storedToken,
+    remainingTime,
+  };
+};
+
 export const AuthContextProvider = (props) => {
-  const initialToken = localStorage.getItem('token');
+  const storedData = retrieveStoredData();
+  let initialToken;
+
+  if (storedData.token) {
+    initialToken = storedData.token;
+  }
+
   const [token, setToken] = useState(initialToken);
   const userIsLoggedIn = !!token;
 
   const logoutHandler = () => {
     setToken(null);
     localStorage.removeItem('token');
+    localStorage.removeItem('expirationTime');
+
+    if (logoutTimer) {
+      clearTimeout(logoutTimer);
+    }
   };
 
   const loginHandler = (token, expirationTime) => {
     setToken(token);
     localStorage.setItem('token', token);
+    localStorage.setItem('expirationTime', expirationTime);
 
     const remainingTime = calculateRemainingTime(expirationTime);
-    setTimeout(logoutHandler, remainingTime);
+    logoutTimer = setTimeout(logoutHandler, remainingTime);
   };
 
   const setTokenHandler = (token) => {
     setToken(token);
   };
+
+  /* When getting stored data during page refresh, need to work with that data
+    in useEffect and need it to be down here because setting logoutTimer calls
+    upon previously defined logoutHandler function. */
+  useEffect(() => {
+    if (storedData) {
+      logoutTimer = setTimeout(logoutHandler, storedData.remainingTime);
+    }
+  }, [storedData]);
 
   const contextValue = {
     token,
